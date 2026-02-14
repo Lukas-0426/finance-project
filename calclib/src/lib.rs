5eb129f4ecc560d1536d0c084f333843777edf0f
Lukas <iLukasjg11@gmail.com>
2026-02-13 19:45:05 -0500

    inital commit, still planning, building logic

---
+++ calclib/src/lib.rs
@@ -0,0 +1,34 @@
+
+
+use serde::*
+
+
+trait calculator( some_data : T) {
+
+    where T : Seralize + Desearlize
+
+
+    fn calculate_balance() -> i32  {
+        
+       
+    }
+
+
+    fn update_income(income : i32) {
+
+    }
+
+
+    fn update_expensies(expense: i32) {
+    }
+
+    fn one_time_income(income : i32) {
+
+    }
+
+    fn one_time_expense(expense : i32) {
+
+    }
+
+
+}
